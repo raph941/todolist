c748f74d770636b272c90c7d7601e16c10b4e418
raph941 <raph941@gmail.com>
2020-03-10 12:21:18 +0100

    Updates

---
+++ precache-manifest.e05c635335d5a34e50683a68e00dd759.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a32d39c3ae7877bf0624a908329bf6b",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "5256dbc3f9c88db9afb8",
+    "url": "/todolist/static/css/main.797c40ad.chunk.css"
+  },
+  {
+    "revision": "9ab9e15dc5f9ad4bfb25",
+    "url": "/todolist/static/js/2.a6bf9bd2.chunk.js"
+  },
+  {
+    "revision": "a9c9365ea08d10a9c5d21d89203dc091",
+    "url": "/todolist/static/js/2.a6bf9bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5256dbc3f9c88db9afb8",
+    "url": "/todolist/static/js/main.e2281326.chunk.js"
+  },
+  {
+    "revision": "7fafe80542216b6fcd86",
+    "url": "/todolist/static/js/runtime-main.25c05e6a.js"
+  }
+]);
